cda60ff8ff4e3b712afa917dbb94e5e8c0c736aa
Snehal Sonawane <snehalsonawane6551@gmail.com>
2026-07-28 00:46:55 +0530

    fix AIAvatar import casing

--- frontend/src/components/chatwindow.jsx
+++ frontend/src/components/chatwindow.jsx
@@ -3,8 +3,9 @@ import { useEffect, useRef } from "react";
 import AIAvatar from "./AIavatar";
 import ChatBubble from "./chatbubble";
 import TypingIndicator from "./typingindicator";
-export default function ChatWindow({ messages, thinking }) {
 
+
+export default function ChatWindow({ messages, thinking }) {
 
   const bottomRef = useRef(null);
 
@@ -18,57 +19,34 @@ export default function ChatWindow({ messages, thinking }) {
   }, [messages, thinking]);
 
 
-
   return (
 
     <div className="flex-1 overflow-y-auto p-8 space-y-6">
-
-
-      {/* AI Status Avatar */}
 
       <AIAvatar
         state={thinking ? "thinking" : "idle"}
       />
 
 
-
-      {/* Messages */}
-
-      {messages.map((msg, index) => (
+      {messages.map((msg,index)=>(
 
         <ChatBubble
-
           key={index}
-
           sender={msg.sender}
-
           message={msg}
-
         />
 
       ))}
 
 
-
-
-      {/* Typing Animation */}
-
       {thinking && (
-
         <TypingIndicator />
-
       )}
 
 
-
-
-      {/* Auto Scroll */}
-
-      <div ref={bottomRef} />
-
+      <div ref={bottomRef}/>
 
     </div>
 
   );
-
 }
